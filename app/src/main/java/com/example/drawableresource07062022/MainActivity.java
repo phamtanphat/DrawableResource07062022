680f130187848f2d698dd4976f95a5feb51c032d
Phat <phatdroid94@gmail.com>
2022-07-28 21:20:02 +0700

    state list

--- app/src/main/java/com/example/drawableresource07062022/MainActivity.java
+++ app/src/main/java/com/example/drawableresource07062022/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.drawableresource07062022;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    LinearLayout linearClickMe;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        linearClickMe = findViewById(R.id.linear_click_me);
+
+        linearClickMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
